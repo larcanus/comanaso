d63d84cf6867f985398edea13ba5779079c322ef
larcanus <larcanus@yandex.ru>
2025-12-25 17:51:45 +0300

    dialog update table style 2

--- src/components/table/DialogTable.vue
+++ src/components/table/DialogTable.vue
@@ -384,7 +384,13 @@ function handleSearchInput() {
     text-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
 }
 
-.styled-table th,
+.styled-table th {
+    border: 1px solid rgba(102, 126, 234, 0.2);
+    padding: 6px 12px 6px 12px;
+    text-align: center;
+    user-select: none;
+}
+
 .styled-table td {
     border: 1px solid rgba(102, 126, 234, 0.2);
     padding: 0 12px 0 12px;
